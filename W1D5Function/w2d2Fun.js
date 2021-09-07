a0084c8c437b47dfd2a6d43d1da0fbdb6a162563
woubishet2311 <84479520+abateWT@users.noreply.github.com>
2021-09-06 20:50:42 -0500

    Update w2d2Fun.js

--- W1D5Function/w2d2Fun.js
+++ W1D5Function/w2d2Fun.js
@@ -10,13 +10,11 @@ console.log("expect 12",double(6));
 console.log("expect -42",double(-21));
 console.log("expect -34",double(-17));
 
-
- /*Q#2.Write a function times100 that takes a number and returns 100 times the number.
+/*Q#2.Write a function times100 that takes a number and returns 100 times the number.
                           Defining Table
  Input                  Process                Output
  A number          100 times the number     100 times the number = 100*number
  */
-
 
 function times100(num){
     return 100*num;
@@ -24,7 +22,6 @@ function times100(num){
 console.log("expect 900",times100(9));
 console.log("expect 0",times100(0));
 console.log("expect -2700",times100(-27));
-
 
 /*
 Q#3. Write a function, myMap, that takes an array and a function and returns a new array that has
@@ -35,39 +32,31 @@ double and times100 functions.
  An array & function          100 times the number       A new array applied to the function
 */
 
-
-const array = [-10,0,10,20];
-function myMap(array,double,times100) {
-   for(let value of array){
-       console.log(array[value]);
-       double();
-       console.log(array[value]);
-   }
+function myMap(array, double) {
+      let result = [];
+ for (let i = 0; i < array.length; i++) {
+      result.push(double(array[i]));
+  }
+console.log(result);
 }
-function double(array){
-     console.log(array[value]);
-     console.log(2*array[value]);
-     times100()
-     console.log(array[value]);
-
+function double(array) {
+   return times100(array * 2)
 }
-function times100(array){
-    console.log(100*array[value]);
+function times100(array) {
+   return array * 100;
 }
-myMap();
+myMap("expect [-20,0,20,40]",[-10,0, 10,20],double);
 
 
 /*
 Q# 4. Demonstrate your myMap function with an anonymous function that triples the input value.
 Write this as an anonymous function expression. Then write it using an arrow expression
 */
-
-
-arr = [-10,0,10,20];
-let myMap = function (arr) {
-for(let elements of arr){
-    console.log( 3*arr[elements]);
-   };
+const arr = [-10,0,10,20];
+for (var i = 0; i < array.length; i++) {
+    var myMap = function () {
+console.log(3 * array[i]);
+    }
 }
 myMap();
 
@@ -75,10 +64,11 @@ myMap();
 // Using arrow expressions
 arr = [-10,0,10,20];
 let myMap = (arr) => {
-   for (let elements of arr) {
-      return (3 * arr[elements]);
+for (let elements of arr) {
+    return (3 * arr[elements]);
    };
 };
 console.log(myMap(arr));
+
 
 module.exports = {double,times100,myMap};

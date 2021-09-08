64a305ac8c89365ef8e402f41a3a98fcc2d429ce
woubishet2311 <84479520+abateWT@users.noreply.github.com>
2021-09-07 21:23:11 -0500

    Update calcuTest.js

--- w2d3Objects/calculator.js/calcuTest.js
+++ w2d3Objects/calculator.js/calcuTest.js
@@ -1,1 +1,46 @@
+"use strict";
+/* global assert calculator */
 
+/* comment out the following for the browser */
+const assert = require("assert");  //always need this with node
+const myExports = require("./calculator.js");  //with node need the name of your file with your functions here
+const calculator = myExports.calculator;  //do this for all of the functions used in the Mocha tests
+
+
+describe("test calculator with values 2, 3", function () {
+    calculator.setValues(2, 3);  //values for the tests
+
+    it("checks initial values 2 and 3", function () {
+        assert.strictEqual(calculator.a, 2);
+        assert.strictEqual(calculator.b, 3);
+    });
+
+    it("when 2 and 3 are entered, the sum is 5", function () {
+        assert.strictEqual(calculator.sum(), 5);
+    });
+
+    it("when 2 and 3 are entered, the product is 6", function () {
+        assert.strictEqual(calculator.mul(), 6);
+    });
+});
+describe("accumulator", function () {
+    it("checks initialValue and increment", function () {
+        const accumulator = new Accumulator(5, 10);
+        assert.strictEqual(accumulator.currentValue, 5);
+        assert.strictEqual(accumulator.increment, 10);
+    });
+
+    it("checks accumulate", function () {
+        const accumulator = new Accumulator(5, 10);
+        accumulator.accumulate();
+        accumulator.accumulate();
+        assert.strictEqual(accumulator.currentValue, 25);
+        assert.strictEqual(accumulator.increment, 10);
+    });
+
+    it("checks report", function () {
+        const accumulator = new Accumulator(5, 10);
+        assert.strictEqual(accumulator.report(), 5);
+    });
+
+});

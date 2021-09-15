128df8b76ea9e15a8d6b49070124b20715e36642
woubishet2311 <84479520+abateWT@users.noreply.github.com>
2021-09-15 14:45:45 -0500

    Update AssgTree.js

--- W3D2NodeTrees.js/nodeassg.js/treeassg.js/AssgTree.js
+++ W3D2NodeTrees.js/nodeassg.js/treeassg.js/AssgTree.js
@@ -1,1 +1,26 @@
+function TreeNode(value) {
+    this.value = value;
+    this.descendents = [];
+}
+   // create nodes with values
+const abe = new TreeNode('Abe');
+const homer = new TreeNode('Homer');
+const bart = new TreeNode('Bart');
+const lisa = new TreeNode('Lisa');
+const maggie = new TreeNode('Maggie');
+// associate root with is descendents
+abe.descendents.push(homer);
+homer.descendents.push(bart, lisa, maggie);
 
+function contains(tree,name){
+    if(tree.value===name)
+        return true;
+    if(tree.descendents && tree.descendents.length>0){
+        for(let item of tree.descendents){
+            console.log(item)
+            if(contains(item,name))
+                return true;
+        }
+    }
+    return false;
+}

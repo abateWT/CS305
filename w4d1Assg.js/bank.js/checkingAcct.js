54eea39f88f87eee93022abd1307e257d2fba437
woubishet2311 <84479520+abateWT@users.noreply.github.com>
2021-09-23 17:01:33 -0500

    Update checkingAcct.js

--- w4d1Assg.js/bank.js/checkingAcct.js
+++ w4d1Assg.js/bank.js/checkingAcct.js
@@ -1,1 +1,30 @@
+"use strict";
+class  CheckingAccount extends Account{
+    constructor(number,overDraft){
+        super(number);
+        this.setOverdraft(overDraft);
+    }
+    setOverdraft(overDraft){
+        this._overDraft=overDraft;
+    }
+    getOverdraft(){
+        return this._overDraft;
+    }
 
+    toString(){
+        return "CheckingAccount "+this._number+ ":"+" balance: "+ this._balance+" overdraft limit: "+this._overDraft
+    }
+    withdraw(amount) {
+        if (amount <= -1*this._overDraft) {
+            throw new RangeError("Insufficient funds");
+        }
+        this._balance -= amount;
+    }
+    endOfMonth(){
+      if(this._balance<=0)
+       return "Warning, low balance "+this._number+":"+ "balance: "+this._balance+" overdraft limit: " +this._overDraft;
+    return super.endOfMonth();
+    }
+  }
+
+//exports.CheckingAccount = CheckingAccount;
